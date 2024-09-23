0c03ac0ce04570f545a7f026bd081bcb3b7573b5
Vladislav Kolyagin <vladislavkolyagin@yandex.ru>
2024-09-24 01:36:31 +0300

    add read file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
-public class  Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int countFile = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке!");
+                continue;
+            } else  {
+                System.out.println("Путь указан верно!");
+                countFile++;
+                System.out.println("Это файл номер №" + countFile);
+
+            }
+        }
+
     }
 }
